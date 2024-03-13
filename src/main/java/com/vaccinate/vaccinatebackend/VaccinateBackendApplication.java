38a2e77d1f9e16fabbea0ab5561cff8a7dd09a4d
Tobechi00 <tobechiokaro2013@gmail.com>
2024-03-13 20:43:50 +0100

    initial commit

---
+++ src/main/java/com/vaccinate/vaccinatebackend/VaccinateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaccinate.vaccinatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaccinate/vaccinatebackend/VaccinateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccinate.vaccinatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
